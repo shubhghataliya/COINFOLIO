923b363631fb09b2852f79928eb22406619d2e2e
Smeet Patel <smeetpatel171@outlook.com>
2023-08-22 19:27:15 +0530

    some other changes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ function App() {
     <>
       <h2>hello world .. i am trying to learn git version control system....</h2>
       <h3>this is the second commit to make..just testing the git repo.</h3>
+      <h4>this is for the pushing of repo.</h4>
     </>
   );
 }
