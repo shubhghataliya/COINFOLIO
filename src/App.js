68ed28670cf3be5cc6dea93f0104c693bdc73f88
Smeet Patel <smeetpatel171@outlook.com>
2023-08-22 18:07:14 +0530

    second for testing repo of git

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h2>hello world .. i am trying to learn git version control system....</h2>
+      <h3>this is the second commit to make..just testing the git repo.</h3>
     </>
   );
 }
