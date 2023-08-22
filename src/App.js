475a9b4a08a8c3d469a18c9260dd56856476e315
Smeet Patel <smeetpatel171@outlook.com>
2023-08-22 19:29:49 +0530

    last change

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ function App() {
       <h2>hello world .. i am trying to learn git version control system....</h2>
       <h3>this is the second commit to make..just testing the git repo.</h3>
       <h4>this is for the pushing of repo.</h4>
+      <h5>last change</h5>
     </>
   );
 }
